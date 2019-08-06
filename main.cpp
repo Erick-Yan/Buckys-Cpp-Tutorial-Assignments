e42b5a64c6ac3b6ca360412c5f534ccd894d4beb
Yanster985 <53266317+Yanster985@users.noreply.github.com>
2019-08-06 19:54:09 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int Addthese(int x, int y)
+{
+    int answer = x * y;
+    return answer;
+}
+
+int main()
+{
+    cout << Addthese(40, 2);
+       return 0;
+}
+
